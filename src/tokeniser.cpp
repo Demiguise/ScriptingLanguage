950f2ead54d7496fd0c548e09a0816dbf564f601
Andy <andy.c.hewitson@gmail.com>
2020-07-08 17:44:27 +0200

    Update tokeniser to match current architecture ideas

---
+++ src/tokeniser.cpp
@@ -0,0 +1,10 @@
+#include "tokeniser.h"
+
+Tokeniser::Tokeniser(std::string filePath)
+{
+}
+
+std::pair<std::string, TTokenVec> Tokeniser::Parse()
+{
+  return {"", {}};
+}
--- src/tokeniser.h
+++ src/tokeniser.h
@@ -2,28 +2,58 @@
 #define __TOKENISER_H__
 
 #include <istream>
+#include <fstream>
+#include <string>
+#include <string_view>
+#include <vector>
 
 enum class Token
 {
+  Null,
   EndOfStream,
-  Identifer, //What determines a valid name?
 
-  Scope_Open,
-  Scope_Close,
+  Literal,      // Any string of characters A-Za-z0-9
+  Double_Quote, // " Character
+  Single_Quote, // ' Character
+  Comma,        // , character
 
-  Equals,
-  Addition,
-  Subtraction,
-  Multiply,
-  Divide,
+  Equals,       // =
+  Addition,     // +
+  Subtraction,  // -
+  Multiply,     // *
+  Divide,       // /
+
+  Paren_Open,   // (
+  Paren_Close,  // )
+  Curly_Open,   // {
+  Curly_Close,  // }
+  Bracket_Open, // [
+  Bracket_Close,// ]
+
+  Statement_End,// ;
 };
+
+struct TokenInfo
+{
+  int mLine;
+  int mCharBegin;
+  int mCharEnd;
+  std::string_view mRaw;
+};
+
+using TTokenPair = std::pair<Token, TokenInfo>;
+using TTokenVec = std::vector<TTokenPair>;
 
 class Tokeniser
 {
-public:
-  Tokeniser(std::ifstream& stream);
+private:
+  std::ifstream mStream;
+  int mCurLine = 0;
 
-  Token Next();
+public:
+  Tokeniser(std::string filePath);
+
+  std::pair<std::string, TTokenVec> Parse();
 };
 
 #endif //__TOKENISER_H__
