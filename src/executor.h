06a7d52112857ca2f40ceddac314a981f849abd8
Andy <andy.c.hewitson@gmail.com>
2020-07-08 17:48:07 +0200

    Added executor and stack headers

---
+++ src/executor.h
@@ -0,0 +1,19 @@
+#ifndef __EXECUTOR_H__
+#define __EXECUTOR_H__
+
+#include "tokeniser.h"
+#include "stack.h"
+
+class Executor
+{
+private:
+  Tokeniser mTokeniser;
+  Stack mStack;
+
+public:
+  Executor(std::string scriptPath);
+  
+  bool Execute();
+}
+
+#endif //~__EXECUTOR_H__
---
+++ src/stack.h
@@ -0,0 +1,10 @@
+#ifndef __STACK_H__
+#define __STACK_H__
+
+class Stack
+{
+public:
+  Stack();
+};
+
+#endif //__STACK_H__
