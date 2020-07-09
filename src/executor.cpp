4222232cdbf4d263503045a54016f8c9f3331e02
Andy <andy.c.hewitson@gmail.com>
2020-07-09 19:00:03 +0200

    Parse entire file, added additional line with comment

--- src/executor.cpp
+++ src/executor.cpp
@@ -8,11 +8,8 @@ bool Executor::Execute()
 {
   std::string raw_statement = "";
   TTokenVec tokens;
-  auto err = mTokeniser.Parse(raw_statement, tokens);
-
-  if (err.has_value())
+  while (!mTokeniser.Parse(raw_statement, tokens).has_value());
   {
-    return false;
   }
 
   return true;
