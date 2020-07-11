0181f147fb50e930aa708a9c05992e80f88814a1
Andy <andy.c.hewitson@gmail.com>
2020-07-11 13:54:55 +0200

    Reworking types and variables

--- src/types.cpp
+++ src/types.cpp
@@ -1,14 +1,15 @@
 #include "types.h"
 
 #define OUTPUT_TYPE(type) case type: return #type
-std::string TypeToString(Type type)
+std::string BaseTypeToString(BaseType type)
 {
   switch (type)
   {
-    OUTPUT_TYPE(Type::Int);
-    OUTPUT_TYPE(Type::Bool);
-    OUTPUT_TYPE(Type::String);
-    OUTPUT_TYPE(Type::Float);
+    OUTPUT_TYPE(BaseType::Null);
+    OUTPUT_TYPE(BaseType::Int);
+    OUTPUT_TYPE(BaseType::Bool);
+    OUTPUT_TYPE(BaseType::String);
+    OUTPUT_TYPE(BaseType::Float);
   }
 }
 #undef OUTPUT_TYPE
--- src/types.h
+++ src/types.h
@@ -4,14 +4,33 @@
 #include <string>
 
 //Temporary while we are dealing with JUST POD types
-enum class Type
+enum class BaseType
 {
+  Null,
   Int,
   Bool,
   String,
   Float,
 };
 
-std::string TypeToString(Type type);
+std::string BaseTypeToString(BaseType type);
+
+class Variable
+{
+private:
+  BaseType mBase = BaseType::Null; //Refers to how the data is stored in the union
+
+  union
+  {
+    int i;
+    bool b;
+    float f;
+    std::string str;
+  } mData;
+
+public:
+  Variable() = default;
+  ~Variable() = default;
+};
 
 #endif //~__TYPES_H__
