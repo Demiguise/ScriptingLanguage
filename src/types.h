3196eef4ca31a1d1bd3d44c76c0f860f30c10122
Andy <andy.c.hewitson@gmail.com>
2020-11-08 19:28:15 +0100

    Switch to using shared ptrs for types

--- src/types.h
+++ src/types.h
@@ -4,6 +4,7 @@
 #include "common.h"
 #include <string>
 #include <unordered_map>
+#include <memory>
 
 //All other types in our language are defined from these POD types.
 enum class BaseType
@@ -15,6 +16,10 @@ enum class BaseType
   Float,
 };
 std::string BaseTypeToString(BaseType type);
+
+//Need to forward declare here of the static TType
+class Type;
+using TType = std::shared_ptr<Type>;
 
 class Type
 {
@@ -31,20 +36,20 @@ public:
   size_t SizeOf();
   bool IsVoid() { return mBase == BaseType::Void; }
 
-  static Type Void;
+  static TType Void;
 };
 
 class TypeRegistry
 {
 private:
-  using TRegistry = std::unordered_map<std::string, Type>;
+  using TRegistry = std::unordered_map<std::string, TType>;
   TRegistry mRegistry;
 
 public:
   TypeRegistry();
 
   void RegisterTypedef(BaseType base, std::string_view name);
-  Type& FindType(std::string_view name);
+  TType FindType(std::string_view name);
 };
 
 #endif //~__TYPES_H__
