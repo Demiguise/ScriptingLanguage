b8ecb3b361d219cf21e47ec44650b03e1de5c4fe
Andy <andy.c.hewitson@gmail.com>
2020-11-14 17:07:55 +0100

    Added set for string_view and TVar

--- src/variables.cpp
+++ src/variables.cpp
@@ -37,119 +37,6 @@ Variable::~Variable()
   }
 }
 
-Result<bool> Variable::Set(const Variable& rhs)
-{
-  /*
-    This feels foul at the moment, but I'm sure we can make it better.
-    Should the variable hold the data? Or should the type?
-    If it's the latter, what does the variable do?
-    How should the types be structued? With templates of an enum type?
-    How would a variable store them?
-  */
-  switch (mType->Base())
-  {
-    case BaseType::Void:
-    {
-      return false;
-    }
-    case BaseType::Int:
-    {
-      try
-      {
-        *(int*)mData = *(int*)rhs.mData;
-        return true;
-      }
-      catch (const std::invalid_argument& e)
-      {
-        return false;
-      }
-      catch (const std::out_of_range& e)
-      {
-        return false;
-      }
-    }
-    case BaseType::Bool:
-    {
-      *(bool*)mData = *(bool*)rhs.mData;
-      return true;
-    }
-    case BaseType::String:
-    {
-      *(std::string*)mData = *(std::string*)rhs.mData;
-      return true;
-    }
-    case BaseType::Float:
-    {
-      try
-      {
-        *(float*)mData = *(float*)rhs.mData;
-        return true;
-      }
-      catch (const std::invalid_argument& e)
-      {
-        return false;
-      }
-      catch (const std::out_of_range& e)
-      {
-        return false;
-      }
-    }
-  }
-}
-/*
-Result<void> Variable::Add(const Variable& rhs)
-{
-  switch (mType->Base())
-  {
-    case BaseType::Void:
-    {
-      return false;
-    }
-    case BaseType::Int:
-    {
-      try
-      {
-        *(int*)mData += *(int*)rhs.mData;
-        return true;
-      }
-      catch (const std::invalid_argument& e)
-      {
-        return false;
-      }
-      catch (const std::out_of_range& e)
-      {
-        return false;
-      }
-    }
-    case BaseType::Bool:
-    {
-      return false;
-    }
-    case BaseType::String:
-    {
-      *(std::string*)mData += *(std::string*)rhs.mData;
-      return true;
-    }
-    case BaseType::Float:
-    {
-      try
-      {
-        *(float*)mData += *(float*)rhs.mData;
-        return true;
-      }
-      catch (const std::invalid_argument& e)
-      {
-        return false;
-      }
-      catch (const std::out_of_range& e)
-      {
-        return false;
-      }
-    }
-  }
-}
-*/
-
 //Templated Stuff
 template<class T, T t>
 class Variable_Impl : public Variable
@@ -157,6 +44,18 @@ class Variable_Impl : public Variable
 public:
   Variable_Impl(std::string_view name, TType type, Byte* pDataBlock)
     : Variable(name, type, pDataBlock) {}
+
+  virtual Result<bool> Set(const TVar& rhs) override 
+  {
+    std::string errMessage = Util::Format("No Set operator implemented for [%s]", BaseTypeToString(mType->Base()).c_str());
+    return { VariableError::OperationNotImplemented, errMessage };
+  }
+
+  virtual Result<bool> Set(const std::string_view& data) override 
+  {
+    std::string errMessage = Util::Format("No Set operator implemented for [%s]", BaseTypeToString(mType->Base()).c_str());
+    return { VariableError::OperationNotImplemented, errMessage };
+  }
 
   virtual Result<bool> Add(const TVar& rhs) override 
   {
